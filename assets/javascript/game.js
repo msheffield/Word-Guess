bbb741f782ecfc3fa9c4d45c7001365d5bde6762
msheffield <maxthesheffield@gmail.com>
2019-09-07 08:44:28 -0700

    keyboard design made, game.js still broken

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,65 @@ Javascript for Word-Guess game.
 */
 
 var game = {
-    word: "hello",
-    correct_guesses: [],
-    wrong_guesses: [],
+    words: [],
+    wins: 0,
+    loses: 0,
 
-    guess: function(char) {
-        if (this.word.includes(char)) {
-            console.log(char + " in word")
-            this.correct_guesses.push(char);
+    startRound: function () {
+        word = wordGen(this.words);
+        this.words.push(word)
+
+        correct_guesses = [];
+        wrong_guesses = [];
+
+        while ((correct_guesses.length < word.length) & (wrong_guesses.length < 9)) {
+            document.onkeyup(event)
+            input = event.key.toUpperCase();
+            if (uniquechar(input, correct_guesses, wrong_guesses) & guess(word, input)) {
+                correct_guesses.push(input);
+                console.log(input + " is in word")
+            }
+            else {
+                wrong_guesses.push(input);
+                console.log(input + " is not in word")
+            }
+        
+
+        }
+
+        if (correct_guesses.length = word.length) {
+            this.wins++;
         }
         else {
-            this.wrong_guesses.push(char)
-            console.log(char + " not in word")
+            this.loses++;
         }
+    }
+}
+
+function uniquechar(input, array1, array2) {
+    if (!(array1.includes(input)) & !(array2.includes(input))) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function wordGen(pastWords) {
+    word = "hello";
+    return word;
+}
+
+function guess(word, char) {
+    if (word.includes(char)) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
 
 game = game;
 
-game.guess("a");
-game.wrong_guesses.forEach(element => {
-    console.log(element);
-});
+//game.startRound()
